8a28ef1295fb370f591470e3fc05220f0f3cfd16
ImPrisonMike <rainier.sarreal@huskers.unl.edu>
2019-04-25 23:01:40 -0500

    Changed the Node class to TreeNode and added getType and getDepth functions.

--- src/PerfectBST.java
+++ src/PerfectBST.java
@@ -1,73 +1,87 @@
+import java.math.BigInteger;
+import java.util.Comparator;
+
 /**
  * This program produces a report from a perfect binary tree.
  * 
  * @author Rainier Sarreal and Andrew Hossack
+ * @param <T>
  */
 
-public class PerfectBST {
+public class PerfectBST<T extends Comparable<T>>  {
+	
+	public class TreeNode<T> {
+		private final T key;
+		TreeNode<T> parent;
+		TreeNode<T> leftChild;
+		TreeNode<T> rightChild;
 
-	Node root;
-
-	// add nodes recursively
-	private Node addRecursive(Node currentNode, int value) {
-		if (currentNode == null) {
-			return new Node(value);
-		}
-		if (value < currentNode.getValue()) {
-			currentNode.leftChild = addRecursive(currentNode.leftChild, value);
-		} else if (value > currentNode.getValue()) {
-			currentNode.rightChild = addRecursive(currentNode.rightChild, value);
-		} else {
-			return currentNode;
-		}
-		return currentNode;
-	}
-
-	// constructor methods
-	PerfectBST(int key) {
-		root = new Node(key);
-	}
-
-	PerfectBST() {
-		root = null;
-	}
-
-	PerfectBST generateTree(int n) {
-		// has to validate n based on the given equation 2^(n+1) -1
-
-		PerfectBST tree = new PerfectBST();
-
-		tree.root = new Node(1);
-
-		for (int i = 2; i <= n; i++) {
-			tree.addRecursive(tree.root, i);
-		}
-
-		return tree;
-	}
-
-	public class Node {
-		private int key;
-		Node leftChild;
-		Node rightChild;
-
-		Node(int key) {
+		TreeNode(T key, TreeNode<T> parent) {
 			this.key = key;
+			this.parent = parent;
 			rightChild = leftChild = null;
 		}
-
-		public int getValue() {
+		
+		public T getValue() {
 			return key;
 		}
+		
+		public void setParent(TreeNode<T> parent) {
+			this.parent = parent;
+		}
+		
+		public TreeNode<T> getParent() {
+			return parent;
+		}
 
-		public Node getLeftChild() {
+		public void setLeftChild(TreeNode<T> leftChild) {
+			this.leftChild = leftChild;
+		}
+		
+		public TreeNode<T> getLeftChild() {
 			return leftChild;
 		}
 
-		public Node getRightChild() {
-			return rightChild;
+		public void setRightChild(TreeNode<T> rightChild) {
+			this.rightChild = rightChild;
 		}
 
+		public TreeNode<T> getRightChild() {
+			return rightChild;
+		}
 	}
-
+	
+	private TreeNode<T> root;
+    private final Comparator<T> cmp;
+    
+    public PerfectBST(Comparator<T> cmp) {
+        this.cmp = cmp;
+    }
+	
+	public String getType(TreeNode<T> node) {
+		TreeNode<T> parent = node.getParent();
+		String type = null;
+		if(node.getValue().compareTo(parent.getValue()) > 0) {
+			type = "RIGHT";
+		} else if(node.getValue().compareTo(parent.getValue()) < 0) {
+			type = "LEFT";
+		} else {
+			type = "ROOT";
+		}
+		return type;
+	}
+	
+	public BigInteger getDepth(TreeNode<T> node) {
+		if(node == root) {
+			return new BigInteger("0");
+		}
+		TreeNode<T> currentNode = node;
+		BigInteger depthCount = BigInteger.ZERO;
+		while(currentNode != root) {
+			currentNode = currentNode.getParent();
+			depthCount = depthCount.add(BigInteger.ONE);
+		}
+		return depthCount;
+	}
+	
 }
