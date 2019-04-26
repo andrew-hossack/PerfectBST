dd68a0535f840542b9b8bfee2856f48bfd7c7db3
Andrew <andrew_hossack@outlook.com>
2019-04-25 20:06:43 -0500

    Constructor and framework methods implemented for Node and PerfectBST classes

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,24 @@
 
 public class Node {
+	private int key;
+	Node leftChild;
+	Node rightChild;
+
+	Node(int key) {
+		this.key = key;
+		rightChild = leftChild = null;
+	}
+
+	public int getValue() {
+		return key;
+	}
+
+	public Node getLeftChild() {
+		return leftChild;
+	}
+
+	public Node getRightChild() {
+		return rightChild;
+	}
 
 }
--- src/PerfectBST.java
+++ src/PerfectBST.java
@@ -1,4 +1,44 @@
 
 public class PerfectBST {
+	
+	Node root;
+	//add nodes recursively
+	private Node addRecursive(Node currentNode, int value) {
+		if (currentNode == null) {
+			return new Node (value);
+		}
+		if (value < currentNode.getValue()) {
+			currentNode.leftChild = addRecursive(currentNode.leftChild, value);
+		} else if (value > currentNode.getValue()) {
+			currentNode.rightChild = addRecursive(currentNode.rightChild, value);
+		} else {
+			return currentNode;
+		}
+		return currentNode;
+	}
+	
+	//constructor methods
+	PerfectBST(int key){
+		root = new Node(key);
+	}
+	
+	PerfectBST(){
+		root = null;
+	}
+	
+	
+	public static void main(String[] args) {
+		PerfectBST tree = new PerfectBST(); 
+		
+		tree.root = new Node(1);
+		
+		for(int i=2; i<100; i++) {
+			tree.addRecursive(tree.root, i);			
+		}
+		
+		
+		
+	}
+	
 
 }
