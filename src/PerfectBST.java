dbf38fceff27fe9b152d8e1733e1f55ad63a4a00
Andrew <andrew_hossack@outlook.com>
2019-04-26 16:10:28 -0500

    New commit

--- src/PerfectBST.java
+++ src/PerfectBST.java
@@ -1,3 +1,4 @@
+
 /**
  * This program produces a report from a perfect binary tree.
  * 
@@ -5,83 +6,69 @@
  * @param <T>
  */
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.util.Comparator;
 
-public class PerfectBST<T extends Comparable<T>>  {
-	
-	public class TreeNode<T> {
-		private final T key;
-		TreeNode<T> parent;
-		TreeNode<T> leftChild;
-		TreeNode<T> rightChild;
+public class PerfectBST<T extends Comparable<T>> {
 
-		TreeNode(T key, TreeNode<T> parent) {
-			this.key = key;
-			this.parent = parent;
-			rightChild = leftChild = null;
+	public static void main(String[] args) {
+		// convert n and key to biginteger types
+		BigInteger n = new BigInteger(args[0].toString());
+		BigInteger key = new BigInteger(args[1].toString());
+
+		// error checking for n, key
+		if ((Math.log(n.add(new BigInteger("1"))) / Math.log(2)) - 1 != (int) (Math.log(n.intValue() + 1) / Math.log(2)) - 1) {
+			System.err.println("Invalid entry for n");
+			return;
 		}
-		
-		public T getValue() {
-			return key;
-		}
-		
-		public void setParent(TreeNode<T> parent) {
-			this.parent = parent;
-		}
-		
-		public TreeNode<T> getParent() {
-			return parent;
+		if (1 > key.intValue() || key.intValue() > n.intValue()) {
+			System.err.println("Invalid entry for key");
+			return;
 		}
 
-		public void setLeftChild(TreeNode<T> leftChild) {
-			this.leftChild = leftChild;
-		}
-		
-		public TreeNode<T> getLeftChild() {
-			return leftChild;
-		}
+		// begin output
+		System.out.println("Tree size n = " + n.toString());
+		System.out.println("\tKey = " + key.toString());
+		System.out.println("\tType = ");
+		System.out.println("\tDepth = " + getDepth(n, key));
+		System.out.println("\tHeight = " + getHeight(n, key));
+		System.out.println("\tParent = ");
+		System.out.println("\tLeft = ");
+		System.out.println("\tRight = ");
 
-		public void setRightChild(TreeNode<T> rightChild) {
-			this.rightChild = rightChild;
-		}
-
-		public TreeNode<T> getRightChild() {
-			return rightChild;
-		}
-	} 
-	
-	private TreeNode<T> root;
-    private final Comparator<T> cmp;
-    
-    public PerfectBST(Comparator<T> cmp) {
-        this.cmp = cmp;
-    }
-	
-	public String getType(TreeNode<T> node) {
-		TreeNode<T> parent = node.getParent();
-		String type = null;
-		if(node.getValue().compareTo(parent.getValue()) > 0) {
-			type = "RIGHT";
-		} else if(node.getValue().compareTo(parent.getValue()) < 0) {
-			type = "LEFT";
-		} else {
-			type = "ROOT";
-		}
-		return type;
+		return;
 	}
-	
-	public BigInteger getDepth(TreeNode<T> node) {
-		if(node == root) {
-			return new BigInteger("0");
+
+	public static int getDepth(BigInteger n, BigInteger key) {
+
+		int depth = 1;		
+		BigInteger currentN = n.add(BigInteger.ONE).divide(new BigInteger("2")); 
+		BigInteger x = n.add(BigInteger.ONE).divide(new BigInteger("2"));
+		
+		if(n.compareTo(key) == 0) {
+			return 0;
 		}
-		TreeNode<T> currentNode = node;
-		BigInteger depthCount = BigInteger.ZERO;
-		while(currentNode != root) {
-			currentNode = currentNode.getParent();
-			depthCount = depthCount.add(BigInteger.ONE);
+		while (currentN.compareTo(key) != 0) {
+
+			if (key.compareTo(currentN) > 0) {
+				currentN = currentN.add(x.divide(BigDecimal.valueOf(Math.pow(2, depth)).toBigInteger())); 
+				depth++;
+			}
+
+			if (key.compareTo(currentN) < 0) {
+				currentN = currentN.subtract(x.divide(BigDecimal.valueOf(Math.pow(2, depth)).toBigInteger())); 
+				depth++;
+			}
+
 		}
-		return depthCount;
+
+		return depth-1;
 	}
-	
+
+	public static int getHeight(BigInteger n, BigInteger key) {
+		double totalDepth = Math.log(n.intValue() + 1) / Math.log(2) - 1;
+		int depthOfKey = getDepth(n, key);
+		return (int) (totalDepth - depthOfKey);
+	}
+
 }
