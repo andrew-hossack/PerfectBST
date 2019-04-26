ccff644c5339f195339f06d3e046b1698b772063
Andrew <andrew_hossack@outlook.com>
2019-04-26 17:22:26 -0500

    getDepth and getHeight methods implemented. Implemented output

--- src/PerfectBST.java
+++ src/PerfectBST.java
@@ -17,12 +17,15 @@ public class PerfectBST<T extends Comparable<T>> {
 		BigInteger key = new BigInteger(args[1].toString());
 
 		// error checking for n, key
-		if ((Math.log(n.add(new BigInteger("1"))) / Math.log(2)) - 1 != (int) (Math.log(n.intValue() + 1) / Math.log(2)) - 1) {
+		if ((Math.log(n.add(new BigInteger("1")).doubleValue()) / Math.log(2))
+				- 1 != (int) (Math.log(n.add(new BigInteger("1")).doubleValue()) / Math.log(2)) - 1) {
 			System.err.println("Invalid entry for n");
+			System.out.println((Math.log(n.add(new BigInteger("1")).doubleValue()) / Math.log(2))
+				- 1);
 			return;
 		}
-		if (1 > key.intValue() || key.intValue() > n.intValue()) {
-			System.err.println("Invalid entry for key");
+		if (key.compareTo(new BigInteger("1")) < 0 || key.compareTo(n) > 0) {
+			System.err.println("Invalid entry for key: " + key + " value = " + n);
 			return;
 		}
 
@@ -40,33 +43,30 @@ public class PerfectBST<T extends Comparable<T>> {
 	}
 
 	public static int getDepth(BigInteger n, BigInteger key) {
-
-		int depth = 1;		
-		BigInteger currentN = n.add(BigInteger.ONE).divide(new BigInteger("2")); 
+		int depth = 1;
+		BigInteger currentN = n.add(BigInteger.ONE).divide(new BigInteger("2"));
 		BigInteger x = n.add(BigInteger.ONE).divide(new BigInteger("2"));
-		
-		if(n.compareTo(key) == 0) {
+
+		if (n.compareTo(key) == 0) {
 			return 0;
 		}
 		while (currentN.compareTo(key) != 0) {
-
 			if (key.compareTo(currentN) > 0) {
-				currentN = currentN.add(x.divide(BigDecimal.valueOf(Math.pow(2, depth)).toBigInteger())); 
+				currentN = currentN.add(x.divide(BigDecimal.valueOf(Math.pow(2, depth)).toBigInteger()));
 				depth++;
 			}
 
 			if (key.compareTo(currentN) < 0) {
-				currentN = currentN.subtract(x.divide(BigDecimal.valueOf(Math.pow(2, depth)).toBigInteger())); 
+				currentN = currentN.subtract(x.divide(BigDecimal.valueOf(Math.pow(2, depth)).toBigInteger()));
 				depth++;
 			}
-
 		}
-
-		return depth-1;
+		return depth - 1;
 	}
+	
 
 	public static int getHeight(BigInteger n, BigInteger key) {
-		double totalDepth = Math.log(n.intValue() + 1) / Math.log(2) - 1;
+		double totalDepth = Math.log(n.add(BigInteger.ONE).doubleValue()) / Math.log(2) - 1;
 		int depthOfKey = getDepth(n, key);
 		return (int) (totalDepth - depthOfKey);
 	}
